b7028737d93886b13221930f98e030956ee2c440
Aaditya Dhruv <aadityadhruv@mailbox.org>
2025-08-10 22:30:27 -0500

    Add parsing filepath
    - Pass a valid markdown filepath and convert it to HTML

---
+++ README.md
@@ -0,0 +1,7 @@
+### Palpad
+
+A really simple markdown parser + static site generator. 
+
+It supports a small subset of the markdown spec. The goal is to be able to host
+a directory of markdown files, and then use `palpad` to convert the directory
+to valid HTML.
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,26 @@ use std::fs::File;
 use std::io::prelude::*;
 
 use parser::lexer::Lexer;
-use parser::parser::{Parser, AST};
+use clap::Parser;
 
 use crate::types::elements;
 use crate::types::elements::Renderable;
 
+#[derive(clap::Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    //Markdown file path
+    #[arg(short, long)]
+    filepath: String 
+}
+
 fn main() {
-    // let doc = elements::HTML::new();
-    // let doc = doc.render();
-    // let mut file = File::create("out.html").unwrap();
-    // file.write_all(doc.as_bytes()).unwrap();
-    let md = "## Title\nText of a paragraph\n\n##### Second heading\nSome more text";
-    let mut lexer = Lexer::new(md);
+    let args = Args::parse();
+    let md = std::fs::read_to_string(args.filepath);
+    
+    let mut lexer = Lexer::new(md.unwrap().as_str());
     lexer.scan();
-    let mut parser = Parser::new(lexer);
+    let mut parser = parser::parser::Parser::new(lexer);
     parser.parse();
     let root = parser.tree;
     let mut doc = elements::HTML::new();
